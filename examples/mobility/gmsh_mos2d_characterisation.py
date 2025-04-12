5aa53ab591343c50d947c0958aaa1e753738a2a6
Marcosemieng <marcomascaro91@hotmail.com>
2025-04-12 18:03:43 +0800

    charachterisation file udpated

--- examples/mobility/gmsh_mos2d_characterisation.py
+++ examples/mobility/gmsh_mos2d_characterisation.py
@@ -1,7 +1,12 @@
+# This '.py' file serves the sole purpose of side dvelopment
+# The used file must go in 'examples/mobility/gmsh_mos2d_characterisation.py'
+
 import os
 import csv
 import numpy as np
 
+import devsim.python_packages.simple_physics as simple_physics
+import gmsh_mos2d_create as gmsh_mos2d_create
 
 # Function to extract Ion (Vg=Vd) from Id@Vg at Vds=Vdd=1V
 def ion(file_path):
@@ -81,8 +86,87 @@ def ion_off_ratio(file_path):
     return on_off_ratio
 
 
+# Function to extract Imax (Vg=Vd) from Id@Vg at Vds=Vdd=1.5V
+def imax(file_path):
+    Vds_bias = 1  # Vds=1V bias value to filter the data: data must be extracted at this Vds=Vdd=1V
+    voltages = []
+    currents = []
+
+    # Open the CSV file and read the data
+    with open(file_path, mode='r', encoding='utf-8') as csv_file:
+        reader = csv.DictReader(csv_file)  # Use DictReader to access columns by name
+        for row in reader:
+            try:
+                # Process only rows where Vds == Vds_bias
+                if (float(row['Vds']) == Vds_bias) & (row['Title'] == 'Id@V_gate_bias'):
+                    voltages.append(float(row['Vgs']))
+                    currents.append(float(row['Ids']))
+            except KeyError:
+                print("Error: Required columns ('Vgs', 'Vds', 'Ids') not found in the CSV file.")
+                return None
+            except ValueError:
+                print("Error: Invalid data in 'Vgs' or 'Vds' or 'Ids' columns.")
+                return None
+
+    # Convert lists to NumPy arrays
+    voltages = np.array(voltages)
+    currents = np.array(currents)
+
+    # Use NumPy's interpolation to extrapolate current at Vgs = 0 and Vgs = 1.5
+    current_at_1 = np.interp(1.5, voltages, currents)
+    return current_at_1
+
+
+# Function to extract Imin (Vg=-1.5) from Id@Vg at Vds=Vdd=1V
+def imin(file_path):
+    Vds_bias = 1  # Vds=1V bias value to filter the data: data must be extracted at this Vds=Vdd=1V
+    voltages = []
+    currents = []
+
+    # Open the CSV file and read the data
+    with open(file_path, mode='r', encoding='utf-8') as csv_file:
+        reader = csv.DictReader(csv_file)  # Use DictReader to access columns by name
+        for row in reader:
+            try:
+                # Process only rows where Vds == Vds_bias
+                if (float(row['Vds']) == Vds_bias) & (row['Title'] == 'Id@V_gate_bias'):
+                    voltages.append(float(row['Vgs']))
+                    currents.append(float(row['Ids']))
+            except KeyError:
+                print("Error: Required columns ('Vgs', 'Vds', 'Ids') not found in the CSV file.")
+                return None
+            except ValueError:
+                print("Error: Invalid data in 'Vgs' or 'Vds' or 'Ids' columns.")
+                return None
+
+    # Convert lists to NumPy arrays
+    voltages = np.array(voltages)
+    currents = np.array(currents)
+
+    # Use NumPy's interpolation to extrapolate current at Vgs = 0 and Vgs = 1
+    current_at_min_Vg = np.interp(-1.5, voltages, currents)
+    return current_at_min_Vg
+
+
+# Function to extract Imax/Imin ratio from Id@Vg at Vds=Vdd=1V
+# this is done as these devices to not have a fixed Vth = 0
+def imax_min_ratio(file_path):
+    # Get the I_on and I_off values
+    i_max = imax(file_path)
+    i_min = imin(file_path)
+
+    # Ensure both values are valid
+    if i_max is None or i_min is None or i_min == 0:
+        print("Error: Invalid I_on or I_off values.")
+        return None
+
+    # Calculate the ratio
+    ion_off_ratio = i_max / i_min
+    return ion_off_ratio
+
+
 # Function to extract the threshold voltage Vth from Id@Vg at Vds=Vdd=1V, by ELR method
-def Vth(file_path, Vds_bias=1):
+def Vth_ELR(file_path, Vds_bias=1):
     # Vds_bias = 1  # Vds=1V bias value to filter the data: data must be extracted at this Vds=Vdd=1V
     voltages = []
     currents = []
@@ -120,15 +204,55 @@ def Vth(file_path, Vds_bias=1):
     slope = derivatives[index_gm_peak]
     v_intersect = voltage_at_gm_peak - (current_at_gm_peak / slope)
 
-    # Find Vth
+    # Find Vth: gm_peak method
     v_th = v_intersect - (Vds_bias/2)
 
     return gm_peak, voltage_at_gm_peak, v_intersect, v_th
 
 
+# Function to extract the threshold voltage Vth from Id@Vg at Vds=Vdd=1V, by 'constant current' method
+def Vth_CC(file_path, Vds_bias=1):
+    # Vds_bias = 1  # Vds=1V bias value to filter the data: data must be extracted at this Vds=Vdd=1V
+    voltages = []
+    currents = []
+
+    # Open the CSV file and read the data
+    with open(file_path, mode='r', encoding='utf-8') as csv_file:
+        reader = csv.DictReader(csv_file)  # Use DictReader to access columns by name
+        for row in reader:
+            try:
+                # Process only rows where Vds == Vds_bias
+                if (float(row['Vds']) == Vds_bias) & (row['Title'] == 'Id@V_gate_bias'):
+                    voltages.append(float(row['Vgs']))
+                    currents.append(float(row['Ids']))
+            except KeyError:
+                print("Error: Required columns ('Vgs', 'Vds', 'Ids') not found in the CSV file.")
+                return None
+            except ValueError:
+                print("Error: Invalid data in 'Vgs' or 'Vds' or 'Ids' columns.")
+                return None
+
+    # Convert lists to NumPy arrays
+    voltages = np.array(voltages)
+    currents = np.array(currents)
+
+    # Find Vth: constant current method
+    voltage_at_target_current = None
+    target_current = 1e-9  # TO-DO: convert in A/cm value - (Vgs=Vth@Ids=1.810-9 (A/um)) 
+    if target_current is not None:
+        try:
+            voltage_at_target_current = np.interp(target_current, currents, voltages)
+        except ValueError:
+            print("Error: Target current is out of range.")
+            voltage_at_target_current = None
+
+    return voltage_at_target_current
+
+
 # Function to extract the Subthreshold swing (SS) from Id@Vg at Vds=Vdd=1V
 # This function uses the Vth value calculated by mosfet_Vth_char
 # and the slope at Vth to calculate the subthreshold swing.
+# TO-DO: modify it to work based on the Vth value calculated by the 'constant current' method
 def SS(file_path):
     # Extract voltages and currents from the CSV file
     Vds_bias = 1  # Vds=1V bias value to filter the data
@@ -136,7 +260,7 @@ def SS(file_path):
     currents = []
 
     # Call mosfet_Vth_char to get v_th
-    _, _, _, v_th = Vth(file_path, Vds_bias = 1)
+    _, _, _, v_th = Vth_ELR(file_path, Vds_bias = 1)
 
     if v_th is None:
         print("Error: Unable to calculate Vth.")
@@ -213,13 +337,16 @@ def mobility_fe(file_path, Lch=None, Wch=None, Cins=None):
 # This function is a placeholder and needs to be implemented based on the specific DIBL calculation method.
 # It needs to run across two sets of Vds: = 1V and = 0.1V, extract Vth for both
 # DIBL = Vth(Vds=1V) - Vth(Vds=0.1V)/(Vds=1V - Vds=0.1V)
+# Vth is extracted by the 'constant current method'
 def DIBL(file_path):
 
     # Call mosfet_Vth_char to get v_th
     Vds_bias_high = 1
     Vds_bias_low = 0.1
-    _, _, _, v_th_1V = Vth(file_path, Vds_bias = Vds_bias_high)
-    _, _, _, v_th_100mV = Vth(file_path, Vds_bias = Vds_bias_low)
+    v_th_1V = Vth_CC(file_path, Vds_bias = Vds_bias_high)
+    v_th_100mV = Vth_CC(file_path, Vds_bias = Vds_bias_low)
+    # _, _, _, v_th_1V = Vth_ELR(file_path, Vds_bias = Vds_bias_high)
+    # _, _, _, v_th_100mV = Vth_ELR(file_path, Vds_bias = Vds_bias_low)
 
     if (v_th_1V is None) or (v_th_100mV is None):
         print("Error: Unable to calculate Vth.")
@@ -231,23 +358,109 @@ def DIBL(file_path):
     return dibl
 
 
-# Function to extract Rtot, Rc, Rch, Rsh
+# Function to extract rds from Id@Vd at Vds=x (This is a meta function)
+def rds_arbitrary(file_path,Vds=0.1,Vgs=1):
+    rds = None
+    
+    voltages = []
+    currents = []
+
+    # Open the CSV file and read the data
+    with open(file_path, mode='r', encoding='utf-8') as csv_file:
+        reader = csv.DictReader(csv_file)  # Use DictReader to access columns by name
+        for row in reader:
+            try:
+                # Process only data of Id@Vds for Vgs=xV 
+                if (float(row['Vgs']) == Vgs) & (row['Title'] == 'Id@V_drain_bias'):
+                    voltages.append(float(row['Vds']))
+                    currents.append(float(row['Ids']))
+            except KeyError:
+                print("Error: Required columns ('Vgs', 'Vds', 'Ids') not found in the CSV file.")
+                return None
+            except ValueError:
+                print("Error: Invalid data in 'Vgs' or 'Vds' or 'Ids' columns.")
+                return None
+
+    # Convert lists to NumPy arrays
+    voltages = np.array(voltages)
+    currents = np.array(currents)
+
+    # Use NumPy's interpolation to extrapolate current at Vds = 0.1 and Vgs = xV
+    current_at = np.interp(Vds, voltages, currents)
+
+    # Calculate the resistance
+    rds = Vds / current_at
+
+    return rds
+
+
+# Function to extract Rtot from Id@Vds
+# This function is a placeholder and needs to be implemented based on the specific resistance calculation method.
+# Methodology: Rtot is extracted at Vds=0.1V (linear region)
+# Steps: 
+# 1) From Id@Vgs (Vds=1v) find the threshold voltage by which Vgs=Vth@Ids=1.810-9 (A/um); 
+# 2) Find two overdrive voltages (Vov = Vgs - Vth) to obtain a min & max carrier density of n2D=1e^12 & 1e^13 (Mos2 specific), 
+# by the formula: n2D = Co*(Vgs-Vth)/q; Translate it into Vgs voltages needed.
+# 3) (OPTIONAL) Make sure those Id@Vds curves are available in that range of overdrive voltages (might need to add additional
+# steps in the Id@Vds simulations @Vgs_overdrive_targets)
+# 4) Extract the current of Id@Vds at Vds=0.1V (linear region) and calculate the resistance as Rtot = Vds/Id
+# Do it for the two different overdirve voltages
+# Temporary: for now is calculated only at one overdrive voltage (fixed at Vov = Vgs=1V - Vth)
+def r_tot(file_path,Vds=0.1,Vgs=1):
+
+    Rtot = None
+
+    # Define consants
+    q = 1.6e-19 # C (charge of an electron)
+    eps_0 = simple_physics.eps_0 # F/cm^2 (vacuum permittivity)
+    eps_ox = simple_physics.eps_ox  # oxide permittivity
+    εox = eps_ox * eps_0
+    # TO-DO: this should be given externally
+    d = gmsh_mos2d_create.oxide_thickness # # cm (back gate oxide thickness)
+
+    # Define MoS2 min & max carrier concentration
+    n2D_min = 1e12 # cm^-2 (minimum carrier concentration)
+    n2D_max = 1e13 # cm^-2 (maximum carrier concentration)
+
+    # Define back gate oxide capacitance
+    Co = εox / d # modify it, this is just a plce holder
+
+    # Call mosfet_Vth_CC to get the threshold voltage Vth by the constant 'current method'
+    v_th = Vth_CC(file_path, Vds_bias = 1)
+
+    # Find the Vgs voltages dictaded by the min & max carrier concentration in MoS2 (typical ones)
+    # Place holder function for when double overdrive voltage will be used
+    Vgs_min = v_th + (q*n2D_min)/(Co)
+    Vgs_max = v_th + (q*n2D_max)/(Co)
+
+    # Calculate actual carrier concentration
+    n2D = Co*(Vgs-v_th)/q
+
+    # Calculate Rtot
+    Rtot = rds_arbitrary(file_path,Vds=Vds,Vgs=Vgs)
+
+    return n2D, Rtot
+
+
+# Function to extract Rc, Rch, Rsh
 # This function is a placeholder and needs to be implemented based on the specific resistance calculation method.
 # Rtot at 4 different channel lengths, Rc extracted as the intercept on the y-axis
-# Rch = Rtot - 2Rc; Rsh = Rch / Lch
+# Take the data from an external .csv file with Rtot at different channel lenghts
+# 1) Rc = intercept on the y-axis at L=0
+# 2) Rch = Rtot - 2Rc; 
+# 3) Rsh = Rch / Lch
 # Use small Vds (0.1V?)
-def resistance(file_path):
-    Rtot = None
+def r_tlm(file_path):
     Rc = None
     Rch = None
     Rsh = None
 
-    return Rtot, Rc, Rch, Rsh
+    return  Rc, Rch, Rsh
 
 
 # Function to extract the Channel Mobility from Id@Vg
 # This function is a placeholder and needs to be implemented based on the specific mobility calculation method.
-# ucon = 1/(q * ns * Rsh); ns = (Cin * Vov) / q (near the source; Rsh = sheet resistance extracted through TLM
+# ucon = 1/(q * ns * Rsh); ns = (Cin * Vov)/q (near the source; Rsh = sheet resistance extracted through TLM
 # Rsh is extracted from the TLM
 def mobility_con(file_path):
     dibl = None
@@ -266,18 +479,23 @@ def compute(a=None):
     ion_value = ion(input_file_path)
     ioff_value = ioff(input_file_path)
     ion_off_ratio_value = ion_off_ratio(input_file_path)
-    _, _, _, vth_value = Vth(input_file_path,Vds_bias = 1)
+    imax_value = imax(input_file_path)
+    imin_value = imin(input_file_path)
+    imm_ratio_value = imax_min_ratio(input_file_path)
+    _, _, _, vth_ELR_value = Vth_ELR(input_file_path,Vds_bias = 1)
+    vth_CC_value = Vth_CC(input_file_path,Vds_bias = 1)
     _, ss_value = SS(input_file_path)
     dibl_value = DIBL(input_file_path)
+    n2D, Rtot = r_tot(input_file_path)
 
     # Step 3:  Store parameters in CSV file
     with open(output_file_path, "a", encoding="utf-8") as ofh:
         # Check if the file is empty or doesn't exist (MM)
         if os.stat(output_file_path).st_size == 0: #(MM)
             # Write the header row (MM)
-            ofh.write("ion,ioff,iratio,vth,ss,dibl,res,ufe,mcon\n")
+            ofh.write("ion,ioff,iratio,imax,imin,imm_ratio,vth_ELR,vth_CC,ss,dibl,n2D,Rtot,res,ufe,mcon\n")
         # Write the data row
-        ofh.write('%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % (ion_value,ioff_value,ion_off_ratio_value,vth_value,ss_value,dibl_value,None,None,None))
+        ofh.write('%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % (ion_value,ioff_value,ion_off_ratio_value,imax_value,imin_value,imm_ratio_value,vth_ELR_value,vth_CC_value,ss_value,dibl_value,n2D,Rtot,None,None,None))
         # i += 1; 
 
     # pyexcel.merge_csv_to_a_book(glob.glob(output_file_path)) #, "id_vds.xlsx")
@@ -288,23 +506,27 @@ def compute(a=None):
 ### Test the functions
 ###
 # file_path = '/Users/macbookpro/Desktop/id_vds.csv'  # Replace with the path to your CSV file
-# i_on = mosfet_ion_char(file_path)
-# i_off = mosfet_ioff_char(file_path)
-# on_off_ratio = mosfet_on_off_ratio_char(file_path)
-# gm_peak, voltage_at_gm_peak, v_intersect, v_th = mosfet_Vth_char(file_path)
-# slope_at_v_th, ss = mosfet_SS_char(file_path)
-# uFE = mosfet_mobility_fe_char(file_path)
+# i_on = ion(file_path)
+# i_off = ioff(file_path)
+# on_off_ratio = ion_off_ratio(file_path)
+# gm_peak, voltage_at_gm_peak, v_intersect, v_th = Vth_ELR(file_path)
+# v_th = Vth_CC(file_path)
+# slope_at_v_th, ss = SS(file_path)
+# dibl = DIBL(file_path)
+# n2D, r_tot = r_tot(file_path)
+# uFE = mobility_fe(file_path)
 # test = compute()
 
-# if i_off:
+
+# if ioff:
 #     current_at_0 = i_off
 #     print(f"Ioff at Vgs = 0V: {current_at_0}")
 
-# if i_on:
+# if ion:
 #     current_at_1 = i_on
 #     print(f"Ion at Vgs = 1V: {current_at_1}")
 
-# if on_off_ratio is not None:
+# if ion_off_ratio is not None:
 #     print(f"I_on/I_off Ratio at Vds = 1V: {on_off_ratio}")
 
 # if gm_peak:
@@ -319,6 +541,22 @@ def compute(a=None):
 # if uFE:
 #     print(f"uFE is: {uFE}")
 
+# if vth:
+#     print(f"Vth is: {vth}")
+
+# if vgs_min:
+#     print(f"Vgs_min is: {vgs_min}")
+
+# if vgs_max:
+#     print(f"Vgs_max is: {vgs_max}")
+
+# if n2D:
+#     print(f"n2D is: {n2D:.2e}", "cm^-2") # cm^-2
+
+# if r_tot:
+#     print(f"Rtot is: {r_tot:.2e}", "kΩ*um") # kOhm * um
+
 # if test:
-#     test = compute()
+#     test = StoreParameters_csv()
 #     test
+
